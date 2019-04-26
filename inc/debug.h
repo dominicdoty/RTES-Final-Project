1aacbbc8c55d5a87d544b4a5e46a98abb54e1876
Dominic Doty <doty.dominic@gmail.com>
2019-04-26 00:35:23 -0600

    Moved sec-msec-nsec conversions here

--- inc/debug.h
+++ inc/debug.h
@@ -3,12 +3,15 @@
 
 // If you want the program to print out debugging info
 // put this define in your code before this header is included
-// #define DEBUG 1
+#define DEBUG  1
 #include <stdio.h>
 #include <time.h>
 // This is gross, but it makes it so the compiler always checks the syntax even when not defined
 // This makes it so you can't break debug prints while they're turned off
 #define debug_print(...) do{ if(DEBUG){printf(__VA_ARGS__);}}while(0)
+
+#define NSEC_PER_MSEC 	(1000000)
+#define NSEC_PER_SEC	(1000000000)
 
 inline void debug_print_time(void)
 {
