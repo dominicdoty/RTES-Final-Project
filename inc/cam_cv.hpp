515d63874885bfb57b839724cb5aef4afbb37b2e
Dominic Doty <doty.dominic@gmail.com>
2019-05-03 19:14:34 -0600

    new defines and VideoCapture definition

--- inc/cam_cv.hpp
+++ inc/cam_cv.hpp
@@ -15,11 +15,31 @@
 #define CANNY_LOW_THRESH (60)
 #define CANNY_HIGH_THRESH (3*CANNY_LOW_THRESH)
 
+// resolution of the image
+#define HRES 	(640)
+#define VRES	(480)
+
+// defining the colorscale range (HSV) that is "blue"
+#define LOW_H	(100)
+#define LOW_S	(150)
+#define LOW_V	(0)
+#define HIGH_H	(140)
+#define HIGH_S	(255)
+#define HIGH_V	(255)
+
+// defining the region of interest (ROI). (0,0) is the top-left corner of the image
+#define START_X		(0)
+#define START_Y		(360)	// 360 pixels from the top
+#define WIDTH_X		(640)	// take the whole width of the image
+#define HEIGHT_Y 	(120)	// take the bottom quarter of the image
+
 /*  Function declarations  */
+
+extern cv::VideoCapture cap;
 
 void* cam_lines(void* args);
 
-void* cam_circles(void* args);
+//void* cam_circles(void* args);
 
 
 #endif
